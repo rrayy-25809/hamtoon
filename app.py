207ac283915bc714da5af8b59f8ef9f8e419aac4
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-02-22 19:13:02 +0900

    Changes to be committed: 부트스트랩 적용 	modified:   .gitignore 	modified:   app.py 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	deleted:    server/__pycache__/main.cpython-312.pyc 	modified:   src/main.ts

--- app.py
+++ app.py
@@ -2,27 +2,26 @@ import subprocess
 import sys
 import server.main as server
 
-if __name__ == "__main__":
-    # Vite 앱 빌드
-    try:
-        process = subprocess.run("npm run build", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8')  # Vite 빌드 명령어 실행
-        
-        # 실시간 로그 출력
-        for line in process.stdout:
-            print(line, end="")
-        for line in process.stderr:
-            print(line, end="")
+# Vite 앱 빌드
+try:
+    process = subprocess.run("npm run build", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8')  # Vite 빌드 명령어 실행
+    
+    # 실시간 로그 출력
+    for line in process.stdout:
+        print(line, end="")
+    for line in process.stderr:
+        print(line, end="")
 
-        if process.returncode != 0:
-            print("❌ 빌드 실패!")
-            sys.exit(1)  # 빌드 실패 시 프로그램 종료
-    except Exception as e:
-        print(f"🚨 오류 발생: {e}")
-        sys.exit(1)  
+    if process.returncode != 0:
+        print("❌ 빌드 실패!")
+        sys.exit(1)  # 빌드 실패 시 프로그램 종료
+except Exception as e:
+    print(f"🚨 오류 발생: {e}")
+    sys.exit(1)
 
-    # 서버 실행
-    if hasattr(server, "flask"):
-        server.flask.run(debug=True, host="0.0.0.0")
-    else:
-        print("❌ server.main에 flask 객체가 없습니다!")
-        sys.exit(1)
+# 서버 실행
+if hasattr(server, "flask"):
+    server.flask.run(debug=True, host="0.0.0.0")
+else:
+    print("❌ server.main에 flask 객체가 없습니다!")
+    sys.exit(1)
