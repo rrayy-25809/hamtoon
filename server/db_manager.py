70480ce0929cd74f50f7aaf09038685de26341df
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-02-24 13:50:05 +0900

    Changes to be committed: db 연동 	new file:   server/db_manager.py 	modified:   server/main.py

---
+++ server/db_manager.py
@@ -0,0 +1,38 @@
+from sqlite3 import Connection, IntegrityError, Cursor
+
+class db_manager:
+    conn:Connection = None
+    cursor:Cursor = None
+
+    def __init__(self, conn:Connection):
+        self.conn = conn
+        self.cursor = conn.cursor()
+
+    # SQLite 데이터베이스 초기화
+    def init_db(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                "id" TEXT PRIMARY KEY,
+                "password" TEXT,
+                "nickname" TEXT,
+                "bookmark" TEXT
+            );
+        ''')
+        self.conn.commit()
+
+    def get_user_info(self, user_id:str):
+        self.cursor.execute("SELECT * FROM users WHERE id = ?", (user_id))
+        
+    
+    def login(self, user_id:str, password:str) -> bool:
+        self.cursor.execute("SELECT * FROM users WHERE id = ? AND password = ?", (user_id, password))
+        return self.cursor.fetchone()
+
+    def signup(self, user_id:str, password:str, nickname:str) -> bool:
+        try:
+            self.cursor.execute("INSERT INTO users (id, password, nickname) VALUES (?, ?, ?)", (user_id, password, nickname))
+            self.conn.commit()
+            self.cursor.close()
+            return True
+        except IntegrityError:
+            return False
--- server/main.py
+++ server/main.py
@@ -1,22 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
+from db_manager import db_manager
+import api_request
 
 flask = Flask(__name__, template_folder='../dist', static_folder='../dist/assets')
-conn = sqlite3.connect('server/db/user.db', check_same_thread=False)
-cursor = conn.cursor()
 flask.secret_key = 'LN$oaYB9-5KBT7G'
-
-# SQLite 데이터베이스 초기화
-def init_db():
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            "id" TEXT PRIMARY KEY,
-            "password" TEXT,
-            "nickname" TEXT,
-            "bookmark" TEXT
-        );
-    ''')
-    conn.commit()
+db = db_manager(sqlite3.connect('server/db/user.db', check_same_thread=False))
 
 @flask.route("/")
 def main():
@@ -30,10 +19,7 @@ def mypage():
 def login():
     if request.method == "POST":
         user_id = request.form["id"]
-        password = request.form["password"]
-        cursor.execute("SELECT * FROM users WHERE id = ? AND password = ?", (user_id, password))
-        user = cursor.fetchone()
-        if user:
+        if db.login(user_id, request.form["password"]):
             session["user_id"] = user_id
             return redirect(url_for("main"))
         else:
@@ -43,17 +29,17 @@ def login():
 @flask.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
-        user_id = request.form["id"]
-        password = request.form["password"]
-        nickname = request.form["nickname"]
-        try:
-            cursor.execute("INSERT INTO users (id, password, nickname) VALUES (?, ?, ?)", (user_id, password, nickname))
-            conn.commit()
+        if db.signup(request.form["id"], request.form["password"], request.form["nickname"]):
             return redirect(url_for("login"))
-        except sqlite3.IntegrityError:
+        else:
             return "이미 존재하는 아이디입니다.", 400
     return render_template("signup.html")
 
+@flask.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    return redirect(url_for("main"))
+
 if __name__ == '__main__':
-    init_db()
+    db_manager.init_db()
     flask.run(debug=True, host='0.0.0.0')
